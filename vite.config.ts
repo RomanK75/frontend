764254f638fed3c7f38b5be267cdce7f6e40f8ad
Mytth <Mytth75@gmail.com>
2024-10-15 19:08:04 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-      },
-      optipng: {
-        enabled: true,
-      },
-      pngquant: {
-        quality: [0.65, 0.90],
-        speed: 4,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      svgo: {
-        multipass: true,
-      },
-    }),
-  ],
+  build: {
+    assetsDir: 'assets',
+  },
   base: '/frontend'
 })
