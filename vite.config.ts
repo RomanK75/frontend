8d3ebe4aa9a65f4f0264adf242e556175989b95f
Mytth <Mytth75@gmail.com>
2024-10-15 19:30:24 +0700

    cdy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@assets': './src/assets',
+      '@assets': '/src/assets',
     },
   },
-  base: '/frontend'
+  base: '/frontend',
+  assetsInclude: ['**/*.png'],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
